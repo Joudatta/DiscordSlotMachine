93dafee31b53e11a2737e40ea5e51ee0de36b8a3
Joudatta <72181663+Joudatta@users.noreply.github.com>
2021-07-16 22:48:22 -0400

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,7 +5,7 @@ import net.dv8tion.jda.api.JDABuilder;
 public class Bot {
 
     public static String prefix = "!";
-    public static String token = "ODQ3OTY5MjcwNjc5NDcwMTIw.YLFy7A.5tFXLj_RBGI8rZ14Yevd9oX2tw4";
+    public static String token = ""; //I removed the actual token because that would lead to possible exploits
 
     public static void main(String[] args) throws Exception {
         JDA jda = JDABuilder.createDefault(token).build();
